602d5880f6dc0ca288d1820e73f4eacd2bc497a9
Christopher-0118 <k.karantsevich@gmail.com>
2018-02-26 17:59:12 +0300

    quadratic-equation/feature: implement solveEquation method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let stringofArgs = equation.split(' ');
+  let a = +stringofArgs[0];
+  let b = +(stringofArgs[3] + stringofArgs[4]);
+  let c = +(stringofArgs[7] + stringofArgs[8]);
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+    return;
+  } else if ( d === 0) {
+    return -b / 2 * a;
+  } else if (d > 0) {
+    let roots = [];
+    
+    roots.push(Math.round((-b + Math.sqrt(d)) / (2 * a)));
+    roots.push(Math.round((-b - Math.sqrt(d)) / (2 * a)));
+    return roots.sort(function(a, b) {
+      return a - b;
+    });
+  }
 }
